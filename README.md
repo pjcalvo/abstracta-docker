3b588b59cb019154cc8603ed2a25f6a5991b7512
Pablo Calvo <pablillo@MBP-de-2017.fritz.box>
2019-06-17 21:56:37 +0200

    inicial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# abstracta-docker
-abstracta-docker
+# copey-registry-webapp
+'VueJS + Bootstrap + Webpack front end repo for Cope Learning Center'
---
+++ src/main.js
@@ -0,0 +1,41 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import App from './App'
+import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSession from 'vue-session'
+import VueResource from 'vue-resource'
+import browserDetect from 'vue-browser-detect-plugin'
+
+Vue.use(BootstrapVue)
+Vue.use(VueResource)
+Vue.use(VueSession)
+Vue.use(browserDetect)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+
+Vue.mixin({
+  methods: {
+    verifyBrowser: function () {
+      if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        alert('safari o es')
+        return false
+      } else if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1 && navigator.userAgent.toLowerCase().indexOf('73') === -1) {
+        alert('chrome 67')
+        return false
+      }
+      return true
+    }
+  }
+})
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: {App}
+})
